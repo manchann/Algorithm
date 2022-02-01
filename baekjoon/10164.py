841ca77b2d83c1da3513fd04a49b602bcaac2ea8
최재강 <chl8273@naver.com>
2022-02-01 21:48:55 +0900

    격자상의 경로

--- baekjoon/10164.py
+++ baekjoon/10164.py
@@ -29,11 +29,18 @@ else:
 
     for i in range(target_x, n):
         for j in range(target_y, m):
+            if target_x == i and target_y == j:
+                continue
             if i == 0:
                 dp[i][j] = 1
             elif j ==0:
                 dp[i][j] = 1
             else:
-                dp[i][j] = dp[i-1][j] + dp[i][j-1]
+                if target_x >= i:
+                    dp[i][j] = dp[i][j-1]
+                elif target_y >= j:
+                    dp[i][j] = dp[i-1][j]
+                else:
+                    dp[i][j] = dp[i-1][j] + dp[i][j-1]
 
 print(dp[n-1][m-1])
